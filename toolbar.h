860953567feb103d4f5dfe57adcb2b9d48bb2b7e
EbrahimEhab202300518 <s-ebrahim.khalifa@zewailcity.edu.eg>
2024-05-10 22:17:23 +0300

    Update toolbar.h

--- toolbar.h
+++ toolbar.h
@@ -9,7 +9,10 @@ private:
 	int height, width;	
 	string toolbarItemImages[ITM_CNT]; //array to hold paths to images of diffrent items in the toolbar
 	game* pGame;
-
+	//new by ebrahim
+	int live;
+	int score;
+	int level;
 public:
 	toolbar(game* r_pGame);
 	toolbarItem getItemClicked(int x);	//handles clicks on toolbar icons, returns the "enum" of the clicekd item
